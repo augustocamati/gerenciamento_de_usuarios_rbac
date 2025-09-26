0044ac5f3711041e051542901db1616ed254964e
Augusto camati <augustocamati21@gmail.com>
2025-09-26 22:51:24 +0100

    first commit

---
+++ src/main/java/camati/example/gerenciamento_de_usuarios_rbac/GerenciamentoDeUsuariosRbacApplication.java
@@ -0,0 +1,13 @@
+package camati.example.gerenciamento_de_usuarios_rbac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeUsuariosRbacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeUsuariosRbacApplication.class, args);
+	}
+
+}
---
+++ src/test/java/camati/example/gerenciamento_de_usuarios_rbac/GerenciamentoDeUsuariosRbacApplicationTests.java
@@ -0,0 +1,13 @@
+package camati.example.gerenciamento_de_usuarios_rbac;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeUsuariosRbacApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
