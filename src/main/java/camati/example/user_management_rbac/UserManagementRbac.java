de4c0896ed30c1dff17dd6b6671eedaa82175e9f
Augusto camati <augustocamati21@gmail.com>
2025-09-26 23:31:09 +0100

    refact: renomeando o projeto para estar em ingles

--- src/main/java/camati/example/user_management_rbac/UserManagementRbac.java
+++ src/main/java/camati/example/user_management_rbac/UserManagementRbac.java
@@ -1,13 +1,13 @@
-package camati.example.gerenciamento_de_usuarios_rbac;
+package camati.example.user_management_rbac;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GerenciamentoDeUsuariosRbacApplication {
+public class UserManagementRbac {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GerenciamentoDeUsuariosRbacApplication.class, args);
+		SpringApplication.run(UserManagementRbac.class, args);
 	}
 
 }
