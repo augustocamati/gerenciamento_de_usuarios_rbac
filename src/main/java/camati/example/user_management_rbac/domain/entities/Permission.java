c1e5b3c96293a3259723fd469775c6e8ab4828af
Augusto camati <augustocamati21@gmail.com>
2025-09-26 23:31:39 +0100

    feat: criando a entidade de permissions

---
+++ src/main/java/camati/example/user_management_rbac/domain/entities/Permission.java
@@ -0,0 +1,15 @@
+package camati.example.user_management_rbac.domain.entities;
+
+public class Permission {
+
+  private String id;
+  private String name;
+  private String description;
+  
+  public Permission(String name) {
+    this.name = name;
+  }
+
+  
+
+}
