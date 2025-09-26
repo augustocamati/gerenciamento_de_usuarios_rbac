435c57e3d634826ca2b59b8b0bb92f74e7fbe874
Augusto camati <augustocamati21@gmail.com>
2025-09-26 23:46:01 +0100

    feat: criando uma exception global

---
+++ src/main/java/camati/example/user_management_rbac/domain/exception/DomainException.java
@@ -0,0 +1,8 @@
+package camati.example.user_management_rbac.domain.exception;
+
+
+public class DomainException extends RuntimeException {
+    public DomainException(String message) {
+        super(message);
+    }
+} 
