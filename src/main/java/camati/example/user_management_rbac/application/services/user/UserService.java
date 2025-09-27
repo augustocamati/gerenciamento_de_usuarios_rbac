597a6ab0dba2708b799f8d32bcb94ad67ef2228f
Augusto camati <augustocamati21@gmail.com>
2025-09-27 02:19:22 +0100

    feat(services): definir interface UserService

---
+++ src/main/java/camati/example/user_management_rbac/application/services/user/UserService.java
@@ -0,0 +1,28 @@
+package camati.example.user_management_rbac.application.services.user;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+import camati.example.user_management_rbac.application.dto.user.AssignRoleRequestDto;
+import camati.example.user_management_rbac.application.dto.user.CreateUserRequestDto;
+import camati.example.user_management_rbac.application.dto.user.CreateUserResponseDto;
+import camati.example.user_management_rbac.application.dto.user.UpdateUserRequestDto;
+import camati.example.user_management_rbac.application.dto.user.UpdateUserResponseDto;
+import camati.example.user_management_rbac.application.dto.user.UserDto;
+
+public interface UserService {
+
+  CreateUserResponseDto createUser(CreateUserRequestDto request);
+
+  void assignRole(AssignRoleRequestDto request);
+
+  
+  List<UserDto> findAll();
+
+  Optional<UserDto> findByUuid(UUID uuid);
+
+ UpdateUserResponseDto update(UUID uuid, UpdateUserRequestDto updateRatingDto);
+
+  void deleteByUuid(UUID uuid);
+}
