5522a763ec201caffe23c5ee4e773d579c2d3ad0
Augusto camati <augustocamati21@gmail.com>
2025-09-27 01:10:45 +0100

    eat(infrastructure): criar entidde JPA  de permission

---
+++ src/main/java/camati/example/user_management_rbac/infrastructure/persistence/jpa/entities/PermissionJpaEntity.java
@@ -0,0 +1,28 @@
+package camati.example.user_management_rbac.infrastructure.persistence.jpa.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PermissionJpaEntity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+
+  @Column(unique = true, nullable = false)
+  private String name;
+
+  private String description;
+
+  // Getters e Setters
+}
