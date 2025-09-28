6afb4149acd729bd11d0b8d05e6ae30f99ecc145
Augusto camati <augustocamati21@gmail.com>
2025-09-28 23:47:16 +0100

    feat: definindo os retornos do userRepository

--- src/main/java/camati/example/user_management_rbac/infrastructure/persistence/jpa/repositories/UserJpaRepository.java
+++ src/main/java/camati/example/user_management_rbac/infrastructure/persistence/jpa/repositories/UserJpaRepository.java
@@ -6,10 +6,11 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import camati.example.user_management_rbac.domain.repositories.UserRepository;
 import camati.example.user_management_rbac.infrastructure.persistence.jpa.entities.UserJpaEntity;
 
 @Repository
-public interface UserJpaRepository extends JpaRepository<UserJpaEntity, UUID> {
+public interface UserJpaRepository extends JpaRepository<UserJpaEntity, UUID>, UserRepository {
   Optional<UserJpaEntity> findByEmail(String email);
 
 }
