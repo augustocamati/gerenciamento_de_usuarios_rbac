5651832b08ce5302768da56cc23cd7b6aaec0b07
Augusto camati <augustocamati21@gmail.com>
2025-09-30 00:01:12 +0100

    feat: dto de atualizacao de role e objeto de role

---
+++ src/main/java/camati/example/user_management_rbac/application/dto/role/UpdateRoleRequestDto.java
@@ -0,0 +1,37 @@
+package camati.example.user_management_rbac.application.dto.role;
+
+
+public class UpdateRoleRequestDto {
+
+  private String name;
+
+  private String description;
+
+  
+
+  public UpdateUserRequestDto() {
+  }
+
+  public UpdateUserRequestDto(String name, String description) {
+    this.name = name;
+    this.description = description;
+  
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
+}
